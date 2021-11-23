c6777b021ad24dad433b5d7b77be1e58f0911526
Lizeth-Del-Rio <liz-delrio@outlook.com>
2021-11-23 17:52:05 -0500

    arreglo bug less parte 1

--- burguerQueen/src/app/core/shared/services/summary-service.service.ts
+++ burguerQueen/src/app/core/shared/services/summary-service.service.ts
@@ -8,56 +8,64 @@ import { Options } from 'src/app/interfaces/menu.interface';
 export class SummaryService {
   summary: Options[] = [];
 
-  private summarySubject = new BehaviorSubject<Options[]>([]);
+  private orderSubject = new BehaviorSubject<Options[]>([]);
   private totalSubject = new BehaviorSubject<number>(0);
 
-  get summaryAction$(): Observable<Options[]> {
-    return this.summarySubject.asObservable();
+  get finalOrder$(): Observable<Options[]>{
+    return this.orderSubject.asObservable();
   }
 
-  get totalAction$(): Observable<number> {
+  get totalAction$(): Observable<number>{
     return this.totalSubject.asObservable();
   }
 
-  private addToSummary(option: Options): void {
-    const existingProduct = this.summary.find(({id}) => id === option.id);
-    if (existingProduct){
-      existingProduct.qty += 1;
-    }else{
-      this.summary.push({ ...option, qty: 1})
-    }
-    this.summarySubject.next(this.summary);
-  }
-
-  private calcTotal():void {
-    const total= this.summary.reduce((acc, el)=> acc += (el.price * el.qty), 0);
-    this.totalSubject.next(total);
-  }
-
-  updateSummary(option:Options):void{
+  updateSummary(option: Options): void{
     this.addToSummary(option);
     this.calcTotal();
   }
 
-  reset():void{
+  private addToSummary(option: Options): void{
+    const existingPrduct = this.summary.find(({id}) => id === option.id);
+    if(existingPrduct){
+      existingPrduct.qty += 1;
+    }else{
+      this.summary.push({...option, qty: 1})
+    }
+    this.orderSubject.next(this.summary)
+  }
+
+  private calcTotal(): void {
+    const total = this.summary.reduce((acc, el) => acc += (el.price * el.qty), 0);
+    this.totalSubject.next(total);
+  }
+
+  deleteAll(): void {
+    this.orderSubject.next([]);
     this.totalSubject.next(0);
-    this.summarySubject.next([]);
     this.summary = [];
   }
 
-  deleteProduct(id:string): void{
-    const newSummary = this.summary.filter(e => e.id !== id)
-    this.summarySubject.next(newSummary);
-    this.summary = newSummary;
-    this.calcTotal();
-  }
-
-  lessProduct(id:string): void{
-    this.summary.forEach( e =>{
-      if( e.id === id){
-        e.qty -= 1
+  lessProduct(id: string): void {
+    this.summary.forEach(el => {
+      if(el.id === id){
+        el.qty -= 1;
       }
     })
+
+    this.orderSubject.next(this.summary)
+    this.calcTotal();
+
+    this.summary.forEach(el => {
+      if(el.qty === 0){
+        this.deleteProduct(id);
+      }
+    })
+  }
+
+  deleteProduct (id: string): void {
+    const newSummary = this.summary.filter(el => id !== el.id);
+    this.orderSubject.next(newSummary);
+    this.summary = newSummary;
     this.calcTotal();
   }
 
