3a6389e331c1b1277884ed3cb3eecf3601e155b3
Lizeth-Del-Rio <liz-delrio@outlook.com>
2021-11-23 18:18:44 -0500

    final bug less() fix

--- burguerQueen/src/app/public/menu/components/menu-nav/menu-tab-content/menu-tab-content.component.ts
+++ burguerQueen/src/app/public/menu/components/menu-nav/menu-tab-content/menu-tab-content.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Options, Menu } from 'src/app/interfaces/menu.interface';
-import { SummaryService } from 'src/app/core/shared/services/summary-service.service';
-=======
+
 import { SummaryService } from '../../../../../core/shared/services/summary.service'
 import { Menu } from 'src/app/interfaces/menu.interface';
 import { Options } from 'src/app/interfaces/menu.interface';
->>>>>>> pruebas
+
 
 
 @Component({
@@ -16,20 +13,12 @@ import { Options } from 'src/app/interfaces/menu.interface';
 })
 export class MenuTabContentComponent implements OnInit {
 @Input() e!:Menu;
-<<<<<<< HEAD
-=======
-  constructor(private summarySvc: SummaryService) { }
->>>>>>> pruebas
 
 constructor( private summarySvc:SummaryService) {}
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
-  addToSummary(option:Options):void{
-=======
   addToSummary(option: Options): void {
->>>>>>> pruebas
     this.summarySvc.updateSummary(option);
   }
 }
